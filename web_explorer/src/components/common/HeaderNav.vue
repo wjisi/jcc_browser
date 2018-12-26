267c4cb14af0e0319e88dc4a6eac5d9439906d60
wjisi <7042783@qq.com>
2018-12-26 10:54:36 +0800

    refactoring nav_headre component

--- web_explorer/src/components/common/HeaderNav.vue
+++ web_explorer/src/components/common/HeaderNav.vue
@@ -1,57 +1,49 @@
 <template>
   <div id="headerNav">
+    <!-- logo -->
     <div class="headLogo">
-      <i class="iconfont icon-weichain_logo" style="color:#0f3988; font-size: 20px"></i>
+      <img :src="logo" >
+      <span>{{$t("message.homeTitle")}}</span>
     </div>
-    <div class="headContent">
-      <!-- Search part -->
-      <div class="searchWrap">
-        <div style="display:inline;cursor:pointer;" @click="toLogin">
-          <span style="font-size:13px;color:#555;user-select:none;">{{$t("message.login")}}</span>
-          <i class="iconfont icon-login" style="font-size:15px;margin-right:20px;color:#555;"></i>
-        </div>
-        <div style="display:inline-block;white-space:nowrap;">
-          <input type="text" v-model="searchContent" :placeholder="$t('message.searchPlaceholder')">
-          <span class="serachButton" @click="confirmSearch">{{$t("message.search")}}</span>
-        </div>
+    <div style="display:flex;align-items:center;">
+      <!-- login button -->
+      <div style="display:inline-block;white-space:nowrap;cursor:pointer;margin-right:20px;" @click="toLogin">
+        <span style="font-size:13px;user-select:none;">{{$t("message.login")}}</span>
+        <i class="iconfont icon-xiangxiaxianshijiantou" style="font-size:15px;"></i>
       </div>
-      <!-- Nav part -->
-      <div class="headNavbar">
-        <span
-          :class="{'navButton':true,'selectNav':currentNav===item.to}"
-          @click="navClick(item.to)"
-          v-for="(item,index) in navTitles"
-          :key="index"
-          v-show="item.isShow"
-        >{{item.name}}</span>
+      <!-- search input -->
+      <div class="searchWarp">
+        <input type="text" v-model="searchContent" :placeholder="$t('message.searchPlaceholder')">
+        <span class="serachButton" @click="confirmSearch">
+          <i class="iconfont icon-sousuoicon"></i>
+        </span>
       </div>
-    </div>
-    <!-- switch language -->
-    <div class="languageSwitch">
-      <el-popover placement="bottom" trigger="hover" v-model="showLanguage">
-        <div v-for="(item,index) in languageList" :key="index" @click="switchLanguage(index);" class="languageContent">
-          <div style="display:inline-block;min-width:60px;margin-left:15px;">{{item.name}}</div>
-        </div>
-        <div slot="reference" style="width:100px;color:#0f3988;position:relative;top:25px;left:10px;cursor:pointer;">
-          <i class="iconfont icon-wangluo" style="font-size:13.5px;float:left;position:relative;top:2.5px;"></i>
-          <span style="padding-left:4px;float:left;">{{languageList[currentLanguage].name}}</span>
-        </div>
-      </el-popover>
+      <!-- switch language -->
+      <el-dropdown @command="switchLanguage" trigger="click">
+        <span class="el-dropdown-link">{{languageList[currentLanguage].name}}
+          <i class="iconfont icon-yuyanqiehuan"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item :command="item.label" v-for="(item,index) in languageList" :key="index">{{item.name}}</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </div>
   </div>
 </template>
 
 <script>
+import logo from "@/images/logo_NavHead.png";
 var homeTitle = document.getElementById("homepage_title");
 export default {
   name: "headerNav",
   data() {
     return {
+      logo,
       searchContent: "",
       showLanguage: false,
       languageList: {
         zh: { label: "zh", name: "简体中文" },
-        en: { label: "en", name: "English" }
+        en: { label: "en", name: "Englishh" }
       }
     };
   },
@@ -64,14 +56,6 @@ export default {
     },
     currentNav() {
       return this.$store.getters.currentNav;
-    },
-    navTitles() {
-      return [
-        { name: this.$t("message.indexPage"), isShow: true, to: "home" },
-        { name: this.$t("message.blockchain"), isShow: true, to: "block" },
-        { name: this.$t("message.hashInfo"), isShow: true, to: "hash" },
-        { name: this.$t("message.moreInfo"), isShow: true, to: "moreInfo" }
-      ];
     }
   },
   methods: {
@@ -94,8 +78,7 @@ export default {
       }
       alert("跳转搜索结果页");
     },
-    switchLanguage(index) {
-      let lang = this.languageList[index].label;
+    switchLanguage(lang) {
       this.$i18n.locale = lang;
       localStorage.setItem("languageType", lang);
       homeTitle.innerHTML = this.$t("message.homeTitle");
@@ -110,100 +93,76 @@ export default {
   font-size: 14px;
   display: flex;
   align-items: center;
-  min-height: 88px;
   justify-content: space-between;
-  padding: 0 30px;
-  border-bottom: 2px solid #eeeeee;
+  min-height: 70px;
+  padding: 0 70px;
+  background-color: #234ade;
+  color: #f4f4fb;
 }
 .headLogo {
-  width: 20%;
-  min-width: 180px;
-  padding-left: 20px;
-  text-align: left;
-}
-.headContent {
   display: flex;
-  flex-flow: column;
-  width: 80%;
-  min-width: 350px;
-  height: 88px;
-  font-weight: 500;
+  align-items: center;
+  margin-right: 20px;
+  img {
+    width: 40px;
+    height: 40px;
+  }
+  span {
+    display: inline-block;
+    font-size: 18px;
+    color: #f4f4fb;
+    margin-left: 20px;
+    text-align: left;
+  }
 }
-.searchWrap {
-  box-sizing: border-box;
-  width: 100%;
-  padding: 15px 10px 0px 0;
-  text-align: right;
+.searchWarp {
+  display: inline-block;
   white-space: nowrap;
-  align-items: flex-end;
+  margin-right: 40px;
   input {
     float: left;
     width: 320px;
-    height: 30px;
+    height: 40px;
     text-indent: 5px;
-    // outline: none;
     font-size: 14px;
-    border: 1px solid #c2c2c2;
-    border-right: none;
+    border: none;
+    border-radius: 4px 0 0 4px;
+    background-color: #f9f9f9;
   }
   .serachButton {
     display: inline-block;
     min-width: 45px;
-    padding: 0 5px;
-    height: 32px;
-    line-height: 34px;
+    height: 40px;
+    line-height: 40px;
     text-align: center;
-    background-color: rgb(59, 143, 238);
+    background-color: #18c9dd;
     color: #fff;
     user-select: none;
-    border-radius: 0 3px 3px 0;
+    border-radius: 0 4px 4px 0;
     cursor: pointer;
   }
 }
-.headNavbar {
-  box-sizing: border-box;
-  display: flex;
+.el-dropdown-link {
+  display: inline-block;
   white-space: nowrap;
-  align-items: flex-end;
-  justify-content: flex-start;
-  min-width: 400px;
-  height: 50px;
-  .navButton {
-    display: inline-block;
-    height: 29px;
-    min-width: 80px;
-    padding: 0 5px;
+  height: 36px;
+  border: 1px solid #18c9dd;
+  border-radius: 6px;
+  line-height: 38px;
+  padding: 0 10px;
+  color: #fff;
+  cursor: pointer;
+  i {
+    font-size: 8px;
+    margin-left: 8px;
     position: relative;
-    top: 2px;
-    border-bottom: 2px solid #eeeeee;
-    color: #5f5f5f;
-    cursor: pointer;
-    &:hover {
-      color: rgba(39, 101, 218, 0.8);
-      border-bottom: 2px solid #2765dacc;
-    }
-  }
-  .selectNav {
-    color: rgba(39, 101, 218, 0.8);
-    border-bottom: 2px solid #2765dacc;
+    bottom: 1px;
   }
 }
-.languageContent {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 22px;
-  margin: 0;
-  color: #232b34;
-  font-size: 13px;
-  padding: 5px 0;
-  line-height: 22px;
-  border-radius: 2px;
-  cursor: pointer;
-  &:hover {
-    background-color: #d0eaef;
+.el-dropdown-menu {
+  background-color: #fff;
+  .el-dropdown-menu__item {
+    padding: 0 22px;
   }
 }
 </style>
